7141371b94de6f5b3338b7217423d454f96c44e3
Bruno Brandão Silva <72681281+Olympus-dev@users.noreply.github.com>
2021-04-28 13:04:54 -0300

    Html error

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,5 @@ import styles from '../styles/Home.module.css'
 //import { Pessoa } from "../lib/Pessoa.js"
 
 export default function Home(){  
-    return (<!DOCTYPE html><html><head><link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet" /><link href="./main.css" rel="stylesheet" /><title>Document</title></head><body><div class="v4_3"><div class="v4_4"></div><span class="v5_3">Sistema Escolar</span><div class="v8_4"></div><div class="v8_5"></div><span class="v8_1">Identificação do Usuário</span><span class="v8_6">Senha</span><div class="v8_7"></div><span class="v8_8">Remember me</span><div class="v8_9"></div><span class="v8_11">Entrar</span></div></body></html>)
+    return (<html><head><link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet" /><link href="./main.css" rel="stylesheet" /><title>Document</title></head><body><div class="v4_3"><div class="v4_4"></div><span class="v5_3">Sistema Escolar</span><div class="v8_4"></div><div class="v8_5"></div><span class="v8_1">Identificação do Usuário</span><span class="v8_6">Senha</span><div class="v8_7"></div><span class="v8_8">Remember me</span><div class="v8_9"></div><span class="v8_11">Entrar</span></div></body></html>)
 }
